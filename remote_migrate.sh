cb7a5703e2a9203d0fb85cf47a8706e12c308c25
Strovala <strahinja.kovacevic994@gmail.com>
2019-03-06 23:29:48 +0100

    Update requirements

--- remote_migrate.sh
+++ remote_migrate.sh
@@ -3,6 +3,7 @@ ssh root@euve258483.serverprofi24.net /bin/bash << EOF
     cd letsfuk
     git fetch
     git pull
+    pip install -U -r requirements.txt
     export APP_SETTINGS="config.DevelopmentConfig"
     export DATABASE_URL="postgresql://letsfuk:root@localhost/letsfuk"
     python manage.py db upgrade
--- remote_run.sh
+++ remote_run.sh
@@ -3,6 +3,7 @@ ssh root@euve258483.serverprofi24.net /bin/bash << EOF
     cd letsfuk
     git fetch
     git pull
+    pip install -U -r requirements.txt
     export APP_SETTINGS="config.DevelopmentConfig"
     export DATABASE_URL="postgresql://letsfuk:root@localhost/letsfuk"
     FLASK_APP=app.py flask run -h 0.0.0.0 -p 5001
