dcc7d298f7fc8b088ba34b9701907100066ed900
Strovala <strahinja.kovacevic994@gmail.com>
2019-04-02 23:21:11 +0200

    Added allow cors headers

--- letsfuk/handlers/__init__.py
+++ letsfuk/handlers/__init__.py
@@ -10,6 +10,13 @@ class BaseHandler(RequestHandler, SessionMixin):
     def set_default_headers(self):
         """Set the default response header to be JSON."""
         self.set_header("Content-Type", 'application/json; charset="utf-8"')
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "session-id")
+        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+
+    def options(self, *args, **kwargs):
+        self.set_status(204)
+        self.finish()
 
     def send_response(self, data, status=200):
         """Construct and send a JSON response with appropriate status code."""
