2f7d773ff9243d388d58ecadabe62e5378073c4e
Strovala <strahinja.kovacevic994@gmail.com>
2019-08-12 20:32:41 +0200

    Added map exception to getting image

--- letsfuk/handlers/images.py
+++ letsfuk/handlers/images.py
@@ -2,14 +2,15 @@ from letsfuk.decorators import (
     check_session, image_endpoint_wrapper,
     map_exception
 )
-from letsfuk.errors import NotFound
+from letsfuk.errors import NotFound, BadRequest
 from letsfuk.handlers import BaseHandler
-from letsfuk.models.s3 import S3Manager, KeyNotFound
+from letsfuk.models.s3 import S3Manager, KeyNotFound, InvalidPayload
 
 
 class ImagesHandler(BaseHandler):
     @image_endpoint_wrapper()
     @map_exception(out_of=KeyNotFound, make=NotFound)
+    @map_exception(out_of=InvalidPayload, make=BadRequest)
     @check_session()
     def get(self):
         S3Manager.verify_key(self.request.params)
